4278882d189681680bab4e806a6af1d9e26bf85e
claudioborges98 <60949457+claudioborges98@users.noreply.github.com>
2020-06-14 22:49:17 -0300

    Update database.js

--- database.js
+++ database.js
@@ -7,10 +7,10 @@ const Pool = require('pg').Pool;
 //3 - Fechar a conexão
 
 const pool = new Pool({  
-    user: 'agqzleqpmhqcoa', 
-    password: 'd5583d20f7351fa10fe3ab7134a36bd199b831e2997de581cacf8415a0b3a68e',
-    host: 'ec2-34-202-88-122.compute-1.amazonaws.com',
-    database: 'd4nrv8gk9ouvue',
+    user: 'jteollvdukwjec', 
+    password: '5dbda661dd3496e43e31634542e0fa066cca59c8a4eae412028f1d0d72a8ce3e',
+    host: 'ec2-34-197-141-7.compute-1.amazonaws.com',
+    database: 'd6jecnuorpjmjt',
     port: '5432',
     ssl: { rejectUnauthorized: false }
 });
